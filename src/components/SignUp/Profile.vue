efaba8fb98c11f9ba7f643b16ef821a88e37ff84
keeley215 <33330633+keeley215@users.noreply.github.com>
2020-03-07 21:01:57 -0500

    Feat/sign up dropdowns (#7)
    Added v-mask
    Added dropdowns
    populated dropdowns

--- src/components/SignUp/Profile.vue
+++ src/components/SignUp/Profile.vue
@@ -5,13 +5,15 @@
           <v-text-field
             placeholder="Phone Number"
             hint="ex: (412) 555-5555"
+            v-mask="phoneNumberMask"
             persistent-hint
             outlined
           /><span class="asterisk">*</span>
         </div>
         <!--Change: DropDown and allign with phone number-->
         <div class="account-field shift-right">
-          <v-text-field
+          <v-autocomplete
+            :items="phoneType"
             placeholder="Phone Type"
             outlined
           /><span class="asterisk">*</span>
@@ -19,14 +21,16 @@
       </div>
       <!--Change: DropDown-->
       <div class="account-field">
-        <v-text-field
+        <v-autocomplete
+          :items="gender"
           placeholder="Gender"
           outlined
         /><span class="asterisk">*</span>
       </div>
       <!--Change: DropDown-->
       <div class="account-field">
-        <v-text-field
+        <v-autocomplete
+          :items="maritalStatus"
           placeholder="Marital Status"
           outlined
         /><span class="asterisk">*</span>
@@ -35,6 +39,7 @@
       <div class="account-field">
         <v-text-field
           placeholder="Date of Birth"
+          v-mask="dateOfBirthMask"
           outlined
         /><span class="asterisk">*</span>
       </div>
@@ -42,8 +47,20 @@
 </template>
 
 <script>
+import { mask } from 'vue-the-mask';
+
 export default {
   name: 'Profile',
+  directives: {
+    mask,
+  },
+  data: () => ({
+    phoneType: ['Mobile', 'Home', 'Work'],
+    gender: ['Female', 'Male'],
+    maritalStatus: ['Married', 'Single', 'Divorced', 'Widowed'],
+    phoneNumberMask: '(###) ###-####',
+    dateOfBirthMask: '##/##/####',
+  }),
 };
 </script>
 
